b4f049920a183f67da43806087634e50e57540df
TamsHallz-128 <tamshalliday21@outlook.com>
2020-06-06 19:27:55 +0100

    username-HNG-06802 script

---
+++ scripts/olajames-HNG-03683.py
@@ -0,0 +1,8 @@
+hngid = 'HNG-03683'
+name = "Afolaranmi James Olaoluwa"
+email = "olaoluwaafolaranmi@gmail.com"
+language = "Python"
+def team_task():
+    print(f"""Hello World, this is {name} with HNGi7 ID {hngid} and email {email} using {language} for stage 2 task""")
+
+team_task()
