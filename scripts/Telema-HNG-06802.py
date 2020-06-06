9729b3a9f35d6365ab5011fdb2e8c8db33c08d47
TamsHallz-128 <tamshalliday21@outlook.com>
2020-06-06 19:53:26 +0100

    Telema-HNG-06802 script

---
+++ scripts/Telema-HNG-06802.py
@@ -0,0 +1,6 @@
+
+name = 'Telema Halliday'
+hng_id = 'HNG-06802'
+language = 'Python'
+email = 'tamshalliday21@outlook.com'
+print("Hello World, this is", name, "with HNGi7 ID", hng_id, "and email", email, "using", language, "for stage 2 task")
